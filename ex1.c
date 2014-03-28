42963801b83bff5830e9a108398d9f150ec472bf
Angela To <to.angelac@gmail.com>
2014-03-28 15:24:50 -0700

    Added pythonex

--- ex1.c
+++ ex1.c
@@ -1,14 +1,16 @@
 /** Example 2-1. A simple OpenCV program that loads an image from disk and 
  *  displays it on the screen
  */
- 
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+using std namespace;
+
 int main( int argc, char** argv ) {
-  IplImage* img = cvLoadImage( argv[1] );
+  IplImage* img = cvLoadImage("~/samples/cat.jpg");
   cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
   cvShowImage( "Example1", img );
   cvWaitKey(0);
